74b052be85fb5a64ed3dc1931a2bf80b4be7f291
Rafał Wróbel <rafalwrobel66@gmail.com>
2024-09-22 15:40:57 +0200

    Add content to setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
+from setuptools import setup
 
+setup(
+    name="WaveDispersion",
+    version="0.1.0",
+    description="desc",
+    author="Rafal Wrobel",
+    author_email="rwrobel@student.agh.edu.pl",
+    python_requires=">=3.8",
+    install_requires=[
+        "scipy>=1.11.4",
+        "matplotlib>=3.8.2"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["wavedispersion"],
+)
